9b0e9bd127dac6325db4f87edf43228ae9ad94fc
Bubcyz <detorresmarc08@gmail.com>
2025-05-07 21:25:05 +0800

    Update cylo.py

--- cylo.py
+++ cylo.py
@@ -1,7 +1,7 @@
 import requests
 import urllib.parse
 
-__ENDPOINT_URL__: str = "https://cpmststudio1.squareweb.app/api"
+__ENDPOINT_URL__: str = "https://cpmstudio1.squareweb.app/api"
 
 class Bubcyz:
     def __init__(self, access_key) -> None:
